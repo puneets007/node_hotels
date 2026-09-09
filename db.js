a074eff3024685d76d77fda923ecbf4e912d120d
puneets007 <puneetsinghal61@gmail.com>
2026-09-09 16:17:55 +0530

    Shift MongoDB to online server

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURL = 'mongodb://localhost:27017/hotels' // Replace mydatabase with whatever you want
-
+//const mongoURL = 'mongodb://localhost:27017/hotels' // Replace mydatabase with whatever you want
+const mongoURL = process.env.MONGODB_URL;
 // Set up MongoDB Connection
 mongoose.connect(mongoURL)
 
--- server.js
+++ server.js
@@ -11,7 +11,7 @@
 const express = require('express');
 const app = express()
 const db = require('./db');
-
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());  // req.body
@@ -31,8 +31,8 @@ const menuRoutes = require('./routes/menuItemRoutes');
 // Use the Routes
 app.use('/person',personRoutes);
 app.use('/menu' , menuRoutes);
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log('listening on port 3000')
 })
